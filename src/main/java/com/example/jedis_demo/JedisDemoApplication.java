6ea7ddc3414bec6e57e97610c4e9902f9749ad4f
yugenquan <1840330817@qq.com>
2019-05-07 16:12:00 +0800

    Initial commit

---
+++ src/main/java/com/example/jedis_demo/JedisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jedis_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JedisDemoApplication.class, args);
+    }
+
+}
